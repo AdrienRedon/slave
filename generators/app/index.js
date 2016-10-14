fc645978caededc020f91cc82e48aac3d749a114
Adrien REDON <redon.adrien@gmail.com>
2016-10-14 15:41:01 +0200

    ADD: argument to set project path

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,31 +2,39 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var _ = require('lodash');
 
 module.exports = yeoman.Base.extend({
+
+  constructor: function () {
+    yeoman.Base.apply(this, arguments);
+
+    // This makes `appname` a required argument.
+    this.argument('appname', { type: String, required: true });
+    // And you can then access it later on this way; e.g. CamelCased
+    this.appname = _.camelCase(this.appname);
+  },
+
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the riveting ' + chalk.red('generator-slave') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    return this.prompt([{
+      type    : 'confirm',
+      name    : 'cool',
+      message : 'Would you like to enable the Cool feature?'
+    }]).then(function (answers) {
+      this.log('app name', answers.name);
+      this.log('cool feature', answers.cool);
     }.bind(this));
   },
 
   writing: function () {
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.destinationPath(`${this.appname}/dummyfile.txt`)
     );
   },
 
